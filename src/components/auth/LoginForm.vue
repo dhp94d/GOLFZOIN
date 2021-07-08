eb41b35e5bada4dae9e484a7338a240ffb983f86
박동현/모바일개발팀 <dhp94d@golfzon.com>
2021-07-08 15:54:06 +0900

    feat: 로그인 폼 컴포넌트 구현

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="contents">
+		<div>
+			<form @submit.prevent="submitForm" class="form">
+				<div>
+					<label for="username">id:</label>
+					<input id="username" type="text" v-model="email" />
+				</div>
+				<div>
+					<label for="password">pw:</label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<button
+					:disabled="!isUsernameValid || !password"
+					type="submit"
+					class="btn btn btn-primary"
+				>
+					로그인
+				</button>
+			</form>
+			<p class="log">{{ logMessage }}</p>
+		</div>
+	</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
+<style scoped></style>
