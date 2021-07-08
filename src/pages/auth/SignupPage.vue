045f08113ae2fdbc2adbc8a485815eb40be498d7
박동현/모바일개발팀 <dhp94d@golfzon.com>
2021-07-08 15:54:17 +0900

    feat: 회원가입  페이지 정의

---
+++ src/pages/auth/SignupPage.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="auth-page">
+		<h3>회원가입 페이지</h3>
+		<SignupForm></SignupForm>
+	</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import SignupForm from '@/components/auth/SignupForm.vue';
+
+export default defineComponent({
+	components: {
+		SignupForm,
+	},
+});
+</script>
+
+<style>
+.auth-page {
+	display: block;
+	justify-content: center;
+	align-items: center;
+	padding: 10px 20px;
+	background-color: white;
+	border: 0.5px solid gray;
+	z-index: 2;
+	box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.05);
+}
+</style>
