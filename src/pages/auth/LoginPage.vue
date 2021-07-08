aee10a95f0e7ebfbfe8c15fca0506dc1b16f179f
박동현/모바일개발팀 <dhp94d@golfzon.com>
2021-07-08 15:53:52 +0900

    feat: 로그인 페이지 정의

---
+++ src/pages/auth/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="auth-page">
+		<div>로그인 페이지</div>
+		<LoginForm></LoginForm>
+	</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import LoginForm from '@/components/auth/LoginForm.vue';
+
+export default defineComponent({
+	components: {
+		LoginForm,
+	},
+});
+</script>
+
+<style></style>
