b70d18125f3332f9f4015096fb170cbf9847bddc
박동현/모바일개발팀 <dhp94d@golfzon.com>
2021-07-15 14:43:47 +0900

    feat: onlineJoin path 구현

---
+++ src/pages/user/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layout">
+    <UserNav></UserNav>
+    <Chat></Chat>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import UserNav from '@/components/user/UserNav.vue';
+import Chat from '@/components/user/Chat.vue';
+
+export default defineComponent({
+  components: {
+    UserNav,
+    Chat,
+  },
+});
+</script>
+
+<style>
+.layout {
+  padding-top: 3rem;
+}
+</style>
