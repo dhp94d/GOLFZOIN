409ef4b4ea6a76b963014f86cd6a6efc3c0870a9
박동현/모바일개발팀 <dhp94d@golfzon.com>
2021-07-15 14:45:00 +0900

    feat: router에 온라인조인, 오프라인조인, 유저 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import MainPage from '@/pages/MainPage.vue';
+import MainPage from '@/pages/index.vue';
 import Login from '@/pages/auth/LoginPage.vue';
 import Signup from '@/pages/auth/SignupPage.vue';
+import User from '@/pages/user/index.vue';
+import Calendar from '@/pages/calendar/index.vue';
+import OfflineJoin from '@/pages/offlineJoin/index.vue';
+import OnlineJoin from '@/pages/onlineJoin/index.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,6 +24,26 @@ const router = createRouter({
       path: '/signup',
       name: 'Signup',
       component: Signup,
+    },
+    {
+      path: '/user',
+      name: 'User',
+      component: User,
+    },
+    {
+      path: '/calendar',
+      name: 'Calendar',
+      component: Calendar,
+    },
+    {
+      path: '/offlineJoin',
+      name: 'OfflineJoin',
+      component: OfflineJoin,
+    },
+    {
+      path: '/onlineJoin',
+      name: 'OnlineJoin',
+      component: OnlineJoin,
     },
   ],
 });
