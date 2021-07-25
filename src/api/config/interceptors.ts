edcd2a6c7a29a35ccfcdfc1586ed09b2a515c7cd
donghyun <68668924+parkdit94@users.noreply.github.com>
2021-07-26 06:29:32 +0900

    feat:interceptors 구현

---
+++ src/api/config/interceptors.ts
@@ -0,0 +1,21 @@
+import { useStore } from 'vuex';
+import { getAuthFromCookie } from '@/composable/cookies';
+
+function setInterceptors(instance: any) {
+  const store = useStore();
+  instance.interceptors.request.use(
+    (config: any) => {
+      config.headers.Authorization =
+        store.getters['userToken'] || getAuthFromCookie();
+      return config;
+    },
+    (error: any) => Promise.reject(error.response)
+  );
+  instance.interceptors.response.use(
+    (config: any) => config,
+    (error: any) => Promise.reject(error.response)
+  );
+  return instance;
+}
+
+export { setInterceptors };
