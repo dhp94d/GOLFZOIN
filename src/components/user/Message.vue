a6abb3eabc40f6a70663b8d3edfbc76430aa019f
박동현/모바일개발팀 <dhp94d@golfzon.com>
2021-07-26 17:08:48 +0900

    feat: chat message 구현

--- src/components/user/Message.vue
+++ src/components/user/Message.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="chat-box" v-for="message in messages" :key="message.id">
-      <div class="name">{{ message.name }}</div>
+      <div class="chat-name">{{ message.name }}</div>
       <div class="chat-data">
-        <div class="text">{{ message.content }}</div>
-        <p>{{ message.timestamp }}</p>
+        <div class="chat-text">{{ message.content }}</div>
+        <div class="chat-time">{{ message.timestamp }}</div>
       </div>
     </div>
   </div>
@@ -18,4 +18,27 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.chat-box {
+  display: block;
+  padding: 1rem 1rem 0rem 1rem;
+}
+.chat-name {
+  font-weight: 600;
+  padding: 0 0.5rem 0.1rem 0.5rem;
+}
+.chat-text {
+  border: 1px solid black;
+  border-radius: 1rem;
+  padding: 0.5rem;
+}
+.chat-data {
+  display: flex;
+}
+.chat-time {
+  position: relative;
+  align-content: bottom;
+  padding-top: 1rem;
+  padding-left: 0.5rem;
+}
+</style>
