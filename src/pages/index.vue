d9ba6cad3bc0cdb023575f5109dfea59e9ba9254
박동현 <dhp94d@golfzon.com>
2021-07-27 14:37:15 +0900

    fix: header height size 고정 해제

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,26 +1,37 @@
 <template>
   <div>
-    <div class="header-page">
+    <div class="header-contianer">
       <Header></Header>
+      <div class="Banner-contianer">
+        <Banner></Banner>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/common/Header.vue';
+import Banner from '@/components/main/Banner.vue';
 
 export default {
   components: {
     Header,
+    Banner,
   },
 };
 </script>
 
 <style scoped>
-.header-page {
+.header-contianer {
   width: 100%;
-  height: 75rem;
+  height: 100%;
   background-image: url('../assets/background.jpg');
   background-size: cover;
+}
+.Banner-contianer {
+  display: block;
+  position: relative;
+  bottom: 0;
+  background-color: white;
 }
 </style>
