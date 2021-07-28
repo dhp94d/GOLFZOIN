3033182ea92462a22cd1cac68a0fe346928c251d
박동현 <dhp94d@gmail.com>
2021-07-28 10:14:47 +0900

    feat: join.vue컴포넌트 구현

---
+++ src/components/join/Join.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      <div>제목 입니다</div>
+    </div>
+    <div>
+      <div>캐러셀 입니다.</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
