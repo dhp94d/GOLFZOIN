0229a6308cd1d60ecb1b6aa2d9219bbbbc78e0ad
박동현 <dhp94d@gmail.com>
2021-07-28 15:40:03 +0900

    fix:banner padding 조정

--- src/components/main/Banner.vue
+++ src/components/main/Banner.vue
@@ -4,7 +4,6 @@
       <div>프로골퍼 / 인플루언서들과 함께하는 온라인 조인</div>
       <button>지금 둘러보기</button>
     </div>
-    <div></div>
   </div>
 </template>
 
@@ -27,7 +26,7 @@ export default {
   justify-content: space-between;
   align-content: center;
   font-size: 4rem;
-  padding: 0 14rem 18rem 14rem;
+  padding: 0 10rem 3rem 10rem;
 }
 .banner-title {
   display: block;
