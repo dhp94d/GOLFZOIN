fe81d0e0c157468f191d714993432b5baf8b1007
박동현 <dhp94d@gmail.com>
2021-07-29 15:59:39 +0900

    feat: online join을 볼 수 있는 온라인 조인 페이지 구현

---
+++ src/pages/join/OnlineJoinPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Join></Join>
+  </div>
+</template>
+<script>
+import Join from '@/components/join/Join.vue';
+export default {
+  components: {
+    Join,
+  },
+};
+</script>
+
+<style></style>
