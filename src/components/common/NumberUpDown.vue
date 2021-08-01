5304e9608c260fa45a8ea07df71173eabcc011f3
박동현 <dhp94d@gmail.com>
2021-08-01 16:30:13 +0900

    feat: common  numberUpdown component 추가

---
+++ src/components/common/NumberUpDown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="number-updown-container">
+    <div class="number-updown-mutation">&lt;</div>
+    <input class="number-updown-view" type="text" />
+    <div class="number-updown-mutation">&gt;</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.number-updown-container {
+  display: flex;
+}
+.number-updown-mutation {
+  padding: 0 1rem;
+}
+.number-updown-view {
+  border: 0;
+  width: 5rem;
+  text-align: center;
+}
+</style>
