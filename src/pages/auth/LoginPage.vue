8f955a2c2d63cfb089d78f9211b54d5b179e02ca
박동현 <dhp94d@gmail.com>
2021-08-12 08:53:18 +0900

    feat: login page css 변경

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="auth-page">
-    <h3 class="page-header">로그인 페이지</h3>
-    <LoginForm></LoginForm>
+  <div>
+    <div>
+      <SubHeader></SubHeader>
+    </div>
+    <div class="auth-container">
+      <div class="auth-page">
+        <h3 class="page-header">로그인</h3>
+        <LoginForm></LoginForm>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import LoginForm from '@/components/auth/LoginForm.vue';
+import SubHeader from '@/components/common/SubHeader.vue';
 
 export default defineComponent({
   components: {
     LoginForm,
+    SubHeader,
   },
 });
 </script>
 
-<style></style>
+<style>
+.auth-container {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+}
+</style>
