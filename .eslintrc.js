5a6376100758f7758abd9686c34ad34b7cea1287
박동현 <dhp94d@gmail.com>
2021-08-12 08:40:11 +0900

    feat: 환경구성

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,6 @@ module.exports = {
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'prettier/prettier': 'error',
-		'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
-		'prettier/prettier': ['error', { endOfLine: 'auto'} ],
 	},
 	parserOptions: {
 		parser: 'babel-eslint',
