ed4e3b8c84969ac974b815e3d2c241df04d234dd
박동현 <dhp94d@gmail.com>
2021-08-12 08:51:37 +0900

    feat: main의 온라인 조인 컴포넌트 구현

---
+++ src/components/main/mainOnline.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <div class="main-join-header">
+      <router-link to="/join/onlinejoin">
+        <div class="main-join-title">온라인 조인</div>
+      </router-link>
+      <router-link to="/join/onlinejoin">
+        <div class="main-join-detail">자세히 보기</div>
+      </router-link>
+    </div>
+    <carousel :breakpoints="breakpoints" :settings="settings">
+      <slide v-for="slide in 7" :key="slide">
+        <JoinItem
+          class="carousel__item"
+          :title="onlineJoinData[slide]?.data.title"
+          :thumbnail="onlineJoinData[slide]?.data.thumbnail"
+          :detailText="onlineJoinData[slide]?.data.detailText"
+          :date="onlineJoinData[slide]?.data.date"
+          :time="onlineJoinData[slide]?.data.time"
+          :id="onlineJoinData[slide]?.id"
+          :simple="true"
+        ></JoinItem>
+      </slide>
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  </div>
+</template>
+<script>
+import { getSelectJoin } from '@/api/join';
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import JoinItem from '@/components/join/JoinItem.vue';
+import { ref } from 'vue';
+export default {
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    JoinItem,
+    Navigation,
+  },
+  setup() {
+    const onlineJoinData = ref(['detailText']);
+    (async () => {
+      const res = await getSelectJoin('online');
+      onlineJoinData.value.push(...res.data);
+    })();
+    const settings = {
+      itemsToShow: 1,
+      modelValue: 1,
+      wrapAround: true,
+    };
+    const breakpoints = {
+      100: {
+        itemsToShow: 1,
+      },
+      // 700px and up
+      800: {
+        itemsToShow: 1.4,
+      },
+      1100: {
+        itemsToShow: 2.4,
+      },
+      1400: {
+        itemsToShow: 3.4,
+      },
+      // 1024 and up
+      1700: {
+        itemsToShow: 4.8,
+      },
+    };
+    return {
+      onlineJoinData,
+      settings,
+      breakpoints,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main-join-header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+}
+.main-join-title {
+  font-size: 2rem;
+  font-weight: bold;
+}
+.main-join-detail {
+  font-weight: bold;
+  color: gray;
+  cursor: pointer;
+  margin: auto 0 0 0;
+
+  &:hover {
+    color: black;
+  }
+}
+.carousel__slide > .carousel__item {
+  transform: scale(0.8);
+  opacity: 0.5;
+  transition: 0.5s;
+  padding: 1rem;
+}
+.carousel__slide--visible > .carousel__item {
+  opacity: 1;
+  transform: rotateY(0);
+}
+.carousel__slide--next > .carousel__item {
+  transform: scale(0.8) translate(-10px);
+}
+.carousel__slide--prev > .carousel__item {
+  transform: scale(0.8) translate(10px);
+}
+.carousel__slide--active > .carousel__item {
+  transform: scale(1);
+}
+</style>
