fc4ac15d7d2dad2ac6432db2506359e3eec608a3
박동현 <dhp94d@gmail.com>
2021-08-26 18:13:13 +0900

    feat: chat server api 구현

--- src/api/server/chat.ts
+++ src/api/server/chat.ts
@@ -1,11 +1,31 @@
 import { notLoggedAxios } from '@/api/server/index';
 
 const enterChatRoom = async (roomNo: string) => {
-  return await notLoggedAxios.get(`/api/chat/enterchatroom/${roomNo}`);
+  try {
+    const res = await notLoggedAxios.get(`/api/chat/enterchatroom/${roomNo}`);
+    if ((res.status = 200)) {
+      return res.data;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  // return await notLoggedAxios.get(`/api/chat/enterchatroom/${roomNo}`);
 };
 
 const addMessage = async (roomNo: string, data: object) => {
-  return await notLoggedAxios.get(`/api/chat/enterchatroom/${roomNo}`, data);
+  try {
+    console.log(roomNo, data);
+    const res = await notLoggedAxios.get(
+      `/api/chat/enterchatroom/${roomNo}`,
+      data
+    );
+    if ((res.status = 200)) {
+      return res.data;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  // return await notLoggedAxios.get(`/api/chat/enterchatroom/${roomNo}`, data);
 };
 
 export { enterChatRoom, addMessage };
