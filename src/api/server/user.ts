961f14950b3ac0f9d00e65215933c8e511fa2228
박동현 <dhp94d@gmail.com>
2021-08-26 18:14:01 +0900

    feat: user server api 구현

--- src/api/server/user.ts
+++ src/api/server/user.ts
@@ -2,27 +2,77 @@ import { notLoggedAxios } from '@/api/server/index';
 import { modifyUserDTO, addFollowDTO } from '@/api/dto/userTypes';
 
 const modifyUser = async (data: modifyUserDTO) => {
-  return await notLoggedAxios.patch(`api/user/modifyuser`, data);
+  try {
+    const res = await notLoggedAxios.patch(`api/user/modifyuser`, data);
+    if ((res.status = 200)) {
+      return true;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  // return await notLoggedAxios.patch(`api/user/modifyuser`, data);
 };
 
 const detailUser = async (userId: string) => {
-  return await notLoggedAxios.get(`api/user/detail/${userId}`);
+  console.log(userId);
+  try {
+    const res = await notLoggedAxios.get(`api/user/detail/${userId}`);
+    if ((res.status = 200)) {
+      return res.data;
+    }
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const addFollow = async (data: addFollowDTO) => {
-  return await notLoggedAxios.post(`api/user/addfollow`, data);
+  try {
+    const res = await notLoggedAxios.post(`api/user/addfollow`, data);
+    if ((res.status = 200)) {
+      return true;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  // return await notLoggedAxios.post(`api/user/addfollow`, data);
 };
 
 const getFollowing = async (userId: string) => {
-  return await notLoggedAxios.get(`api/user/following?id=${userId}`);
+  try {
+    const res = await notLoggedAxios.get(`api/user/following?id=${userId}`);
+    if ((res.status = 200)) {
+      return res.data;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  // return await notLoggedAxios.get(`api/user/following?id=${userId}`);
 };
 
 const getFollower = async (userId: string) => {
-  return await notLoggedAxios.get(`api/user/follower?id=${userId}`);
+  try {
+    const res = await notLoggedAxios.get(`api/user/follower?id=${userId}`);
+    if ((res.status = 200)) {
+      return res.data;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  // return await notLoggedAxios.get(`api/user/follower?id=${userId}`);
 };
 
-const findUser = async (keyword: string) => {
-  return await notLoggedAxios.get(`/api/user/finduser?keyword=${keyword}`);
+const findUser = async (keyword: string, userid: string) => {
+  try {
+    const res = await notLoggedAxios.get(
+      `/api/user/finduser?keyword=${keyword}&userid=${userid}`
+    );
+    if ((res.status = 200)) {
+      return res.data;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  // return await notLoggedAxios.get(`/api/user/finduser?keyword=${keyword}`);
 };
 
 export {
